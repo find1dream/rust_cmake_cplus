e780e26a68c629ba0d3cdb9c8a2b4c3bcba6f82f
clinfeng <clinfeng.jlu@gmail.com>
2022-12-06 09:56:32 +0000

    format variable name

--- lib/cmake_project/build.rs
+++ lib/cmake_project/build.rs
@@ -2,8 +2,9 @@ use cmake;
 use bindgen;
 
 fn main(){
+    let cpp_path = String::from("src/cpp");
     // designate path of CMakeLists.txt
-    let dist = cmake::Config::new("src/cpp").build();
+    let dist = cmake::Config::new(&cpp_path).build();
     println!("cargo:rustc-link-search=native={}", dist.display());
 
     // if no other static likbary
@@ -13,15 +14,17 @@ fn main(){
     println!("cargo:rustc-link-lib=dylib=stdc++");
 
     // configure bindgen
+    let wrapper_file = format!("{}/{}", cpp_path, String::from("wrapper.hpp"));
+    let binding_file = String::from("src/bindings.rs");
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=src/cpp/wrapper.hpp");
+    println!("cargo:rerun-if-changed={}", wrapper_file);
 
     let bindings = bindgen::Builder::default()
-        .header("src/cpp/wrapper.hpp")
+        .header(&wrapper_file)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
     bindings
-        .write_to_file("src/bindings.rs")
+        .write_to_file(&binding_file)
         .expect("Couldn't write bindings!");
 }
